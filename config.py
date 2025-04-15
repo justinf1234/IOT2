6ffef6d68881110a5e36e853c7bfe500459e4bf2
justinf1234 <justlandry1@gmail.com>
2025-04-15 15:31:58 -0400

    Made the code fully functional

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ AWS_PRIVATE_KEY = 'certs/aws_private.key'
 
 ################## Subscribe / Publish client #################
 CLIENT_ID = 'fromPi'  
-TOPIC = 'justin/device/raspberry/data' 
+TOPIC = 'justin/device/33/data' 
 OFFLINE_QUEUE_SIZE = -1
 DRAINING_FREQ = 2
 CONN_DISCONN_TIMEOUT = 10
